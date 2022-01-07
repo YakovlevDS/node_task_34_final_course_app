b53e4086620e497132d4c5f6890442daec849846
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-01-07 20:29:03 +0200

    added compression

---
+++ keys/keys.dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  MONGODB_URI:
+    "mongodb+srv://dima:QXWWh3icr5xxw8mV@cluster0.kgiqr.mongodb.net/store",
+  SESSION_SECRET: "some secret value",
+  SENDGRID_API_KEY:
+    "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",
+  EMAIL_FROM: "1d3ddd@gmail.com",
+  BASE_URL: "http://localhost:3000",
+};
